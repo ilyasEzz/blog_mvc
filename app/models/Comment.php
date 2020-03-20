ab6862e0e14179db3740a82ead8b5dc6033f15a5
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-03-20 11:43:56 +0700

    restructuring the public folder

---
+++ app/models/Comment.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Comment {
+  private $model;
+
+  public function __construct() {
+    $this->model = new Model(); 
+  }
+}
--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,6 +1,6 @@
   </div>
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script  src="js/app.js"></script>  
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script  src="assets/js/app.js"></script>  
 </body>
 </html>
